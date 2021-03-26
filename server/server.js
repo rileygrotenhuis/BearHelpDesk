06bf597ae5f0f01cfce3e1d5a371bb7426a2602b
Riley Grotenhuis <rileygrotenhuis@Rileys-MBP-010011065195.MissouriState.edu>
2021-03-26 13:29:01 -0500

    Add middleware to Node application to connect React frontend to Node Backend

--- server/server.js
+++ server/server.js
@@ -6,6 +6,16 @@ const PORT = process.env.PORT || 5000;
 // PostgreSQL Database Connection
 const pool = require('./config/db.config');
 
+// Middleware
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+const cors = require('cors');
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+}));
+
 // Listen
 app.listen(PORT, () => {
     console.log(`Bear Help Desk is running at ${PORT}`);
