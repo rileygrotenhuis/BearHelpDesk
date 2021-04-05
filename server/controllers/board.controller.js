39e9042ecfb4205ce28973f907a309b06b6bba5b
Riley Grotenhuis <rileygrotenhuis@Rileys-MBP-010011065195.MissouriState.edu>
2021-04-05 18:34:28 -0500

    Edge cases.

--- server/controllers/board.controller.js
+++ server/controllers/board.controller.js
@@ -1,7 +1,7 @@
 // PostgreSQL Database Connection
 const pool = require('../config/db.config');
 
-// Get all of the tickets that no employee is currently workign on
+// Get all of the tickets that no employee is currently working on
 exports.getAllTickets = async () => {
     // SQL SELECT Query
     var data = await pool.query("SELECT * FROM tickets WHERE employee IS NULL");
