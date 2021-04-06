5408c4e05b6ee609a8a5ee311373d3c32582ae05
Riley Grotenhuis <rg1050@live.missouristate.edu>
2021-04-06 12:22:49 -0500

    Add possible new database connection to db.config.js file

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -2,12 +2,24 @@
 const Pool = require('pg').Pool;
 
 // Define new PostgreSQL Pool
+
+// OLD CONNECTION
 const pool = new Pool({
     host: 'localhost',
     port: 5432,
     password: '',
     database: 'bearhelpdesk'
 });
+
+
+/* NEW CONNECTION
+const pool = new Pool({
+    host: 'ec2-54-145-102-149.compute-1.amazonaws.com',
+    port: 5432,
+    password: 'd4773495e2719da47d5d5f5b987f279049bb249ff150c27b5a7e4c6b3596e7fb',
+    database: 'dbjmp22s3qu7d7'
+});
+*/
 
 // Export
 module.exports = pool;
