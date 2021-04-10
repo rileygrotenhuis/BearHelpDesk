c7a3b882cc810b9b859277285420bde0fe26a598
Riley Grotenhuis <rileygrotenhuis@Rileys-MacBook-Pro.local>
2021-04-10 15:30:29 -0500

    Small spelling update to Navbar2 component.

--- client/src/Components/Navbar2.js
+++ client/src/Components/Navbar2.js
@@ -31,7 +31,7 @@ function Navbar() {
 
     return (
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <Link to="/dashboard" class="navbar-brand" href="#">Navbar</Link>
+            <Link to="/dashboard" class="navbar-brand">Bear Help Desk</Link>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
