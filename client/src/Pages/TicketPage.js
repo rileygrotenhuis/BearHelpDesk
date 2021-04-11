db8d10953e7c0fd92431e70876a94206396e0c60
Riley Grotenhuis <rileygrotenhuis@Rileys-MBP-010011065195.MissouriState.edu>
2021-04-11 15:19:19 -0500

    Setup basic functionality for Ticket Page.

--- client/src/Pages/TicketPage.js
+++ client/src/Pages/TicketPage.js
@@ -1,17 +1,46 @@
 // React Modules
 import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
+import Axios from 'axios';
+
+// Components
+import Navbar2 from '../Components/Navbar2';
 
 // Ticket Page
 function TicketPage() {
 
+    // useHistory Variable
+    let history = useHistory();
+
     // useParams Variables
     const { ticketID } = useParams();
+
+    // On Page Load
+    useEffect(() => {
+        // GET => '/login'
+        Axios({
+            method: "GET",
+            withCredentials: true,
+            url: 'http://localhost:5000/login'
+        }).then(results => {
+            // If there is no logged in user detected, redirect to login page
+            if (results.data.employee_email == null) {
+                alert('You must be logged in to access this page!');
+                history.push('/login');
+            // Otherwise, carry on...
+            } else {
+                alert(`You will soon be able to see the info for ${ticketID}`);
+            }
+        });
+    }, []);
 
     // JSX
     return (
         <div>
-            <h1>Ticket Page</h1>
+            <Navbar2 />
+            <div className="container mt-3">
+                <h1 className="mb-3">Ticket Page</h1>
+            </div>
         </div>
     );
 }
