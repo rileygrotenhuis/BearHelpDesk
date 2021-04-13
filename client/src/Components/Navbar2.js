dcbd7efa99c347ae965cab012afbc89be12b210e
Riley Grotenhuis <rileygrotenhuis@Rileys-MBP-010011065195.MissouriState.edu>
2021-04-12 22:02:39 -0500

    Small styling changes to Navbar2 component.

--- client/src/Components/Navbar2.js
+++ client/src/Components/Navbar2.js
@@ -32,25 +32,27 @@ function Navbar() {
     // JSX
     return (
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <Link to="/dashboard" class="navbar-brand">Bear Help Desk</Link>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <Link to="/dashboard" class="nav-link" href="#">Dashboard</Link>
-                </li>
-                <li class="nav-item active">
-                    <Link to="/board" class="nav-link" href="#">Board</Link>
-                </li>
-                <li class="nav-item">
-                    <Link to="/profile" class="nav-link" href="#">Profile</Link>
-                </li>
-                <li class="nav-item">
-                    <a className="nav-link" onClick={logout}>Logout</a>
-                </li>
-                </ul>
+            <div className="container-fluid">
+                <Link to="/dashboard" className="navbar-brand">Bear Help Desk</Link>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                    <li className="nav-item active">
+                        <Link to="/dashboard" className="nav-link">Dashboard</Link>
+                    </li>
+                    <li class="nav-item active">
+                        <Link to="/board" className="nav-link">Board</Link>
+                    </li>
+                    <li class="nav-item">
+                        <Link to="/profile" className="nav-link">Profile</Link>
+                    </li>
+                    <li class="nav-item">
+                        <a className="nav-link" onClick={logout}>Logout</a>
+                    </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     )
