0664ad4e375888d4ba80268174d31cf5ae62454c
Riley Grotenhuis <rileygrotenhuis@Rileys-MBP.lan>
2021-04-22 18:04:08 -0500

    Add the minified CSS file to the client side.

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,9 @@ import {
     Route
 } from 'react-router-dom';
 
+// CSS
+import './App.min.css';
+
 // Components
 import HomePage from './Pages/HomePage';
 import SubmitPage from './Pages/SubmitPage';
