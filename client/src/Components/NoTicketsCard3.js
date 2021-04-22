54a0678a4125c0be961048f5b869bd8e31892b49
Riley Grotenhuis <rileygrotenhuis@Rileys-MBP.lan>
2021-04-22 18:33:08 -0500

    Update components for the Profile Page.

--- client/src/Components/NoTicketsCard3.js
+++ client/src/Components/NoTicketsCard3.js
@@ -5,7 +5,10 @@ import React from 'react';
 function NoTicketsCard3() {
     // JSX
     return (
-        <h1>You have not completed any tickets!</h1>
+        <div className="notickets">
+            <h1 className="notickets-title">You have not completed any tickets!</h1>
+            <p className="notickets-text">You will have to complete tickets on your Dashboard Page</p>
+        </div>
     );
 }
 
